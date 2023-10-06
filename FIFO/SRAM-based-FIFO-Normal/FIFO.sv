7616b717194eceef54451dee2c950194793a3981
hansollasido <johnkims222@gmail.com>
2023-10-06 14:18:51 +0900

    chip_select 신호 삭제

--- FIFO/SRAM-based-FIFO-Normal/FIFO.sv
+++ FIFO/SRAM-based-FIFO-Normal/FIFO.sv
@@ -7,8 +7,6 @@ module SRAM_Based_FIFO #(
     input   wire                        clk,
     input   wire                        rst_n,
 
-    input   wire                        chip_select_i,
-
     output  wire                        full_o,
     input   wire                        wren_i,
     input   wire    [DATA_WIDTH-1:0]    wdata_i,
@@ -38,13 +36,11 @@ module SRAM_Based_FIFO #(
         .data_o(rdata_o), //read시 나오는 data
         .wren_i(wren_i), //wren
         .rden_i(rden_i), //rden
-        .rst_n(rst_n), //reset
-        .chip_select_i(chip_select_i) //해당 chip을 사용
+        .rst_n(rst_n) //reset
     );
 
 
     always_ff @(posedge clk)
-        if (chip_select_i) begin
             if (!rst_n) begin
                 full                        <= 1'b0;
                 empty                       <= 1'b1;    // empty after as reset
@@ -60,12 +56,10 @@ module SRAM_Based_FIFO #(
                 rdptr                       <= rdptr_n;
             end
             
-        end
 
     always_comb begin
         wrptr_n                     = wrptr;
         rdptr_n                     = rdptr;
-        if (chip_select_i) begin
             if (wren_i) begin
                 wrptr_n                     = wrptr + 'd1;
             end
@@ -73,7 +67,6 @@ module SRAM_Based_FIFO #(
             if (rden_i) begin
                 rdptr_n                     = rdptr + 'd1;
             end
-        end
         empty_n                     = (wrptr_n == rdptr_n);
         full_n                      = (wrptr_n[DEPTH_LG2]!=rdptr_n[DEPTH_LG2])
                                      &(wrptr_n[DEPTH_LG2-1:0]==rdptr_n[DEPTH_LG2-1:0]);
@@ -117,15 +110,13 @@ module SRAM #(
     output  reg [DATA_WIDTH-1:0]        data_o,
     input   wire                        wren_i,
     input   wire                        rden_i,
-    input   wire                        rst_n,
-    input   wire                        chip_select_i
+    input   wire                        rst_n
 );
 
     reg [DATA_WIDTH-1:0] sram [DATA_DEPTH];
     localparam  FIFO_DEPTH              = (1<<ADDRESS_DEPTH);
 
     always_ff @ (posedge clk)
-        if (chip_select_i) begin 
             if (!rst_n & RST_MEM)begin
                 for (int i=0; i<FIFO_DEPTH; i++) begin
                     sram[i]                      <= {DATA_WIDTH{1'b0}};
@@ -139,7 +130,7 @@ module SRAM #(
                     data_o = sram [read_address_i];
                 end
             end
-        end
+
 
     
 
