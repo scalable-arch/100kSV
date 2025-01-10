f6e76e6063cd3bc6912c406a708811dce65d7e32
박상현 <81350669+sg05060@users.noreply.github.com>
2025-01-10 12:10:06 +0900

    fix indent

--- Arbiter/Round_Robin_Arbiter/RTL/SAL_ARBITER_RR_64TO1.sv
+++ Arbiter/Round_Robin_Arbiter/RTL/SAL_ARBITER_RR_64TO1.sv
@@ -7,7 +7,7 @@ module SAL_ARBITER_RR_64TO1
 #(
     parameter   REQ_CNT                 = 64
   , parameter   REQ_CNT_LG2             = $clog2(REQ_CNT)
-  , parameter	DATA_WIDTH				        = 12
+  , parameter	DATA_WIDTH	        = 12
 )
 (
     input   wire                        clk
@@ -21,86 +21,85 @@ module SAL_ARBITER_RR_64TO1
   , output  logic   [DATA_WIDTH-1:0]   	data_o 
   , input   wire                        gnt_i
 );
-	localparam	DEPTH1_REQ_CNT			      = REQ_CNT >> 2; //16
-	localparam	DEPTH0_REQ_CNT			      = REQ_CNT >> 4; //4
+    localparam	DEPTH1_REQ_CNT		= REQ_CNT >> 2; //16
+    localparam	DEPTH0_REQ_CNT		= REQ_CNT >> 4; //4
 
-	wire	[DEPTH1_REQ_CNT-1:0]		        depth1_req_arr;
-	wire	[DEPTH0_REQ_CNT-1:0]		        depth0_req_arr;
+    wire	[DEPTH1_REQ_CNT-1:0]	depth1_req_arr;
+    wire	[DEPTH0_REQ_CNT-1:0]	depth0_req_arr;
 
-	wire	[DATA_WIDTH-1:0]			          depth1_data_arr [0:DEPTH1_REQ_CNT-1];
-	wire	[DATA_WIDTH-1:0]			          depth0_data_arr [0:DEPTH0_REQ_CNT-1];
+    wire	[DATA_WIDTH-1:0]	depth1_data_arr [0:DEPTH1_REQ_CNT-1];
+    wire	[DATA_WIDTH-1:0]	depth0_data_arr [0:DEPTH0_REQ_CNT-1];
 
-  wire	[DEPTH1_REQ_CNT-1:0]		        depth1_gnt_arr;
-	wire	[DEPTH0_REQ_CNT-1:0]		        depth0_gnt_arr;
+    wire	[DEPTH1_REQ_CNT-1:0]	depth1_gnt_arr;
+    wire	[DEPTH0_REQ_CNT-1:0]	depth0_gnt_arr;
 
-	//------------------------------------------------------------------------------
-	// DEPTH 0 | # of Arbiter = 1
-	//------------------------------------------------------------------------------
-	SAL_ARBITER_RR_4TO1
-	#(
-		  .REQ_CNT                 		      (4)
-	  , .DATA_WIDTH						            (DATA_WIDTH)
-	) 
-	u_arbiter_depth0
-	(
-		  .clk							                (clk)
-	  , .rst_n							              (rst_n)
-	  , .req_arr_i						            (depth0_req_arr)
-	  , .data_arr_i						            (depth0_data_arr)
-	  , .gnt_arr_o						            (depth0_gnt_arr)
-	  , .req_o							              (req_o)
-	  , .data_o							              (data_o)
-    , .gnt_i                            (gnt_i)
-	);
-
-	//------------------------------------------------------------------------------
-	// DEPTH 1 | # of Arbiter = 4
-	//------------------------------------------------------------------------------
-	genvar j;
+    //------------------------------------------------------------------------------
+    // DEPTH 0 | # of Arbiter = 1
+    //------------------------------------------------------------------------------
+    SAL_ARBITER_RR_4TO1
+    #(
+	.REQ_CNT                        (4)
+      , .DATA_WIDTH			(DATA_WIDTH)
+    ) 
+    u_arbiter_depth0
+    (
+	.clk				(clk)
+      , .rst_n				(rst_n)
+      , .req_arr_i			(depth0_req_arr)
+      , .data_arr_i			(depth0_data_arr)
+      , .gnt_arr_o			(depth0_gnt_arr)
+      , .req_o				(req_o)
+      , .data_o				(data_o)
+      , .gnt_i                          (gnt_i)
+     );
+	
+    //------------------------------------------------------------------------------
+    // DEPTH 1 | # of Arbiter = 4
+    //------------------------------------------------------------------------------
+    genvar j;
     generate
         for(j = 0; j < 4; j = j + 1) begin
             SAL_ARBITER_RR_4TO1 
             #(
                 .REQ_CNT                (4)
-              , .DATA_WIDTH				      (DATA_WIDTH)
+              , .DATA_WIDTH		(DATA_WIDTH)
             ) 
             u_arbiter_depth1
             (
-                .clk					          (clk)
-              , .rst_n					        (rst_n)
-              , .req_arr_i				      (depth1_req_arr[(j*4)+:4])
-              , .data_arr_i				      (depth1_data_arr[(j*4)+:4])
-              , .gnt_arr_o				      (depth1_gnt_arr[(j*4)+:4])
-              , .req_o					        (depth0_req_arr[j])
-              , .data_o					        (depth0_data_arr[j])
+                .clk			(clk)
+              , .rst_n			(rst_n)
+              , .req_arr_i		(depth1_req_arr[(j*4)+:4])
+              , .data_arr_i		(depth1_data_arr[(j*4)+:4])
+              , .gnt_arr_o		(depth1_gnt_arr[(j*4)+:4])
+              , .req_o			(depth0_req_arr[j])
+              , .data_o			(depth0_data_arr[j])
               , .gnt_i                  (depth0_gnt_arr[j])
             );
         end
     endgenerate
 
-	//------------------------------------------------------------------------------
-	// DEPTH 2 | # of Arbiter = 16
-	//------------------------------------------------------------------------------
-	genvar i;
+    //------------------------------------------------------------------------------
+    // DEPTH 2 | # of Arbiter = 16
+    //------------------------------------------------------------------------------
+    genvar i;
     generate
         for(i = 0; i < 16; i = i + 1) begin
             SAL_ARBITER_RR_4TO1 
             #(
                 .REQ_CNT                (4)
-              , .DATA_WIDTH				      (DATA_WIDTH)
+              , .DATA_WIDTH		(DATA_WIDTH)
             ) 
             u_arbiter_depth2
             (
-                .clk					          (clk)
-              , .rst_n					        (rst_n)
-              , .req_arr_i				      (req_arr_i[(i*4)+:4])
-              , .data_arr_i				      (data_arr_i[(i*4)+:4])
-              , .gnt_arr_o				      (gnt_arr_o[(i*4)+:4])
-              , .req_o					        (depth1_req_arr[i])
-              , .data_o					        (depth1_data_arr[i])
+                .clk			(clk)
+              , .rst_n			(rst_n)
+              , .req_arr_i		(req_arr_i[(i*4)+:4])
+              , .data_arr_i		(data_arr_i[(i*4)+:4])
+              , .gnt_arr_o		(gnt_arr_o[(i*4)+:4])
+              , .req_o			(depth1_req_arr[i])
+              , .data_o			(depth1_data_arr[i])
               , .gnt_i                  (depth1_gnt_arr[i])
             );
         end
     endgenerate
-
 endmodule
